b27ae0e47678ae13f78cc0d2654e0fd4bcc7be9e
Alexander Yang <65861156+blue-putty@users.noreply.github.com>
2022-07-24 13:21:52 -0400

    implement butler api

---
+++ server/src/types/index.ts
@@ -0,0 +1,12 @@
+export * from './butlerlabs';
+
+export interface File {
+  fieldname: string;
+  originalname: string;
+  encoding: string;
+  mimetype: string;
+  destination: string;
+  filename: string;
+  path: string;
+  size: number;
+}
