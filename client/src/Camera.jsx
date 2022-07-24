1fefd511310b36a854e7b94ed24c9f61203f30d0
Anish Jha <anishj2900@gmail.com>
2022-07-24 18:19:52 -0400

    f u alex n2

--- client/src/Camera.jsx
+++ client/src/Camera.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Webcam from 'react-webcam';
+import axios from 'axios'
 
 const videoConstraints = {
   width: 350,
@@ -10,11 +11,15 @@ const videoConstraints = {
 export default function Camera() {
   const [image, setImage] = useState('');
   const webcamRef = React.useRef(null);
+  let imageSrc = '';
 
   const capture = React.useCallback(() => {
-    const imageSrc = webcamRef.current.getScreenshot();
+    imageSrc = webcamRef.current.getScreenshot();
     setImage(imageSrc);
   });
+  function sendImage() {
+    axios.post("/api/upload", imageSrc);
+  }
 
   return (
     <div className="webcam-container">
@@ -34,19 +39,22 @@ export default function Camera() {
       </div>
       <div className="ImageCam">
         {image !== '' ? (
-          <button
-            type="submit"
-            variant="contained"
-            color="secondary"
-            size="small"
-            onClick={(e) => {
-              e.preventDefault();
-              setImage('');
-            }}
-            className="webcam-btn"
-          >
-            Retake Image
-          </button>
+          <div >
+            <button
+              type="submit"
+              variant="contained"
+              color="secondary"
+              size="small"
+              onClick={(e) => {
+                e.preventDefault();
+                setImage('');
+              }}
+              className="webcam-btn"
+            >
+              Retake Image
+            </button>
+            <button onClick={sendImage} type="submit" className="webcam-btn">Submit Image</button>
+          </div>  
         ) : (
           <button
             type="submit"
